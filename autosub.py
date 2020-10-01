2a52e738b44da0f919509e27fab8863e9b0ff877
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-01 09:55:52 -0500

    Create autosub.py

---
+++ autosub.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import discord
+
+class AutoSub(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_channel_create(channel):
+      if channel.category.name == "MODMAIL":
+        await ctx.invoke(bot.get_command("sub <@&719980372980531201>"))
+
+    @commands.command()
+    async def say(self, ctx, *, message):
+        await ctx.send(message)
+
+def setup(bot):
+    bot.add_cog(AutoSub(bot))
