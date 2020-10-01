9dcd20f6002982d5816ce6018881031a92f5ad81
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-01 16:18:43 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -5,6 +5,7 @@ class AutoSub(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         print('Plugin print line 7')
+        print(ctx)
 
     @commands.Cog.listener()
     async def on_guild_channel_create(self, ctx):
