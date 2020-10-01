f48028f254fbb1d8a5c0a7f4c1a399e1bbb4a0e8
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-01 15:05:17 -0500

    Updated autosub.py No more ande

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -7,6 +7,7 @@ class AutoSub(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_channel_create(channel):
+      # Sub if it's in the MODMAIL Category
       if channel.category.name == "MODMAIL":
         await ctx.invoke(bot.get_command('sub'), user_or_role=ctx.guild.get_role(719980372980531201))
 
