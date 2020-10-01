a92005d2e9ade7f278ee321f29f8741bea4ec665
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-01 16:08:51 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -8,8 +8,13 @@ class AutoSub(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_channel_create(self, ctx):
       # Sub if it's in the MODMAIL Category
-      if channel.category.name == "MODMAIL":
+      if ctx.category.name == "MODMAIL":
+        
+        print('It should sub')
         await ctx.invoke(bot.get_command('sub'), user_or_role=ctx.guild.get_role(719980372980531201))
+      else:
+        print('Wrong category')
+        print(ctx.category.name)
 
     @commands.command()
     async def say(self, ctx, *, message):
