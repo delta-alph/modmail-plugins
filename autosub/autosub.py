761ca23a94cfe9a19f9d87351046a5769b982a27
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-02 12:23:29 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -7,20 +7,20 @@ class AutoSub(commands.Cog):
         print('Plugin print line 7')
 
     @commands.Cog.listener()
-    async def on_guild_channel_create(self, ctx):
+    async def on_guild_channel_create(self, channel):
       # category_id = 719324997461606455 # RMJ
       category_id = 761620853824815175 # Delpha's
       role_id = 729298666296180746 # foo
       # role_id = 719980372980531201 # RMJ Managers
       category = discord.utils.get(self.bot.guild.categories, id=category_id)
       # Sub if it's in the MODMAIL Category
-      if ctx.category.id == category_id:
+      if channel.category.id == category_id:
         print('It should sub')
         # await self.bot.invoke(self.bot.get_command('sub'), user_or_role=ctx.guild.get_role(role_id))
-        await ctx.invoke(ctx.get_command('sub'))
+        await self.bot.invoke(channel.get_command('sub'))
       else:
         print('Wrong category')
-        print(ctx.category.name)
+        print(channel.category.name)
 
     @commands.command()
     async def say(self, ctx, *, message):
