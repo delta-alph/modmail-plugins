055d053734d25ffd0b3ac6f903e32f3a5aa3a747
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-02 11:38:18 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -18,7 +18,8 @@ class AutoSub(commands.Cog):
       print('category_id: ', category_id)
       if ctx.category.id == category_id:
         print('It should sub')
-        await self.bot.invoke(self.bot.get_command('sub'), user_or_role=ctx.guild.get_role(role_id))
+        # await self.bot.invoke(self.bot.get_command('sub'), user_or_role=ctx.guild.get_role(role_id))
+        await self.bot.invoke(self.bot.get_command('sub', user_or_role=ctx.guild.get_role(role_id)))
       else:
         print('Wrong category')
         print(ctx.category.name)
