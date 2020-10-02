6228c6bf1c0c0bdbc3177ecfa532412b1c0ec5cb
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-02 16:29:44 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -7,6 +7,13 @@ class AutoSub(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    async def checkMessages(channel, role_id):
+        messages = await channel.history().flatten()
+        print('Messages: ', messages)
+        first_msg = messages[0]
+        ctx = await self.bot.get_context(first_msg)
+        await ctx.invoke(self.bot.get_command(f'sub <@&{role_id}>'))
+        
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel):
         # category_id = 719324997461606455 # RMJ
@@ -22,13 +29,6 @@ class AutoSub(commands.Cog):
         else:
             print('Wrong category')
             print(channel.category.name)
-            
-    async def checkMessages(channel, role_id):
-        messages = await channel.history().flatten()
-        print('Messages: ', messages)
-        first_msg = messages[0]
-        ctx = await self.bot.get_context(first_msg)
-        await ctx.invoke(self.bot.get_command(f'sub <@&{role_id}>'))
 
     @commands.command()
     async def say(self, ctx, *, message):
