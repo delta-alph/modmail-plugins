77f2e6bd6c557e7411d0076333fb54b0b8174bf4
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-02 13:13:10 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -4,7 +4,7 @@ import discord
 class AutoSub(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel, ctx: commands.Context):
       # category_id = 719324997461606455 # RMJ
