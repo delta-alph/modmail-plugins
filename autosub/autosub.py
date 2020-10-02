408e0774e5d1d63502db669d579b22692f6c3fb6
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-02 17:17:21 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -6,7 +6,7 @@ import asyncio
 class AutoSub(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print('v0.0.007')
+        print('v0.0.008')
     
     async def check_messages(self, channel, role_id):
         messages = await channel.history().flatten()
@@ -31,7 +31,8 @@ class AutoSub(commands.Cog):
             ctx = await self.bot.get_context(first_msg)
             
             # await ctx.invoke(self.bot.get_command(f'sub <@&{role_id}>'))
-            await ctx.invoke(self.bot.get_command('loglink'))
+            # await ctx.invoke(self.bot.get_command('loglink'))
+            await ctx.invoke(bot.get_command('sub'), user_or_role=ctx.guild.get_role(role_id))
         else:
             print('Wrong category')
             print(channel.category.name)
