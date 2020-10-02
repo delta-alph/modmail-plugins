2f4a2fdffa01114984b6b93e2bee79cbcc51692b
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-02 12:51:12 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -2,9 +2,8 @@ from discord.ext import commands
 import discord
 
 class AutoSub(commands.Cog):
-    def __init__(self, bot, ctx):
+    def __init__(self, bot):
         self.bot = bot
-        self.ctx = ctx
 
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel):
