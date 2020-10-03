0a53c4b69de6b134a31ca54d6242a6b93dd55962
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-03 07:25:17 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -7,7 +7,7 @@ from pprint import pprint
 class AutoSub(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print('v0.0.043')
+        print('v0.0.043 ')
         print(self)
         print(self.bot)
         
