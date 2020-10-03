9d29402c57af1107e123ab2fc31dcfd8d50a11a9
delta-alph <63759466+delta-alph@users.noreply.github.com>
2020-10-02 20:29:30 -0500

    Update autosub.py

--- autosub/autosub.py
+++ autosub/autosub.py
@@ -10,10 +10,10 @@ class AutoSub(commands.Cog):
         
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel):
-        # category_id = 719324997461606455 # RMJ
-        category_id = 761620853824815175 # Delpha's
-        role_id = 729298666296180746 # foo
-        # role_id = 719980372980531201 # RMJ Managers
+        category_id = 719324997461606455 # RMJ
+        # category_id = 761620853824815175 # Delpha's
+        # role_id = 729298666296180746 # foo
+        role_id = 719980372980531201 # RMJ Managers
         
         # Sub if it's in the MODMAIL Category
         if channel.category.id == category_id:
